e91aaa5f6ce00b90121388035c26ebe98f291a0f
Fozil0208 <xudoyberdiyev1995fozil@gmail.com>
2024-02-11 23:25:22 +0900

    state

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import Root from "./root.jsx";
+import Root from "./Root.1.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>
 );
- 
--- src/root.jsx
+++ src/root.jsx
@@ -1,25 +1,32 @@
 import { Component } from "react";
 
 class Root extends Component {
+  constructor(props) {
+    spuer(props);
+    this.state = {
+      title: "Webbrain",
+      count: 0,
+    };
+  }
+  onChangeText() {
+    // title = event.target.value
+    this.setState({title: event.target.value, count:1})
+    console.log("this", this);
+    // console.log("event", event.target.value);
+    // console.log("title", title);
+    console.log("======");
+  }
   render() {
     function clickHello(event) {
       console.log("Hello World", event);
     }
 
-    function onChangeText(event) {
-      console.log("typing...", event);
-    }
+    
     return (
       <div>
-        <h1 onClick={clickHello}>Hello World</h1>
-        <button
-          onClick={function () {
-            clickHello;
-          }}
-        >
-          click me
-        </button>
-        <input onChange={onChangeText} type="text" />
+        <h1 onClick={clickHello}>Hello Webbrain {this.state.title}</h1>
+        <button onClick={() => clickHello("Webbrain")}>click me</button>
+        <input onChange={(event) => this.onChangeText(event)} type="text" />
         <select name="" id="">
           <option value="Uz">Uzbek</option>
           <option value="Eng">English</option>
